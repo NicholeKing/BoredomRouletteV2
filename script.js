731ab98f3529b5bef3cdda8b4d3762ba989d5e61
Nichole <nichole.king94@hotmail.com>
2020-04-02 13:03:25 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,340 @@
+var baseList = [
+    "Give yourself a manicure/pedicure", 
+    "Experiment with your hair", 
+    "Draw", 
+    "Do yoga", 
+    "Write a poem, story, or journal entry", 
+    "Take a nap", 
+    "Read", 
+    "Listen to music", 
+    "Find a new band or artist", 
+    "Stretch", 
+    "Watch TV/Streaming service", 
+    "Look at old photos/videos", 
+    "Bake", 
+    "Meditate", 
+    "Have a snack", 
+    "Sit outside (if weather permits)", 
+    "Write a letter to your future self",
+    "Cuddle with someone or something",
+    "Go for a hike",
+    "Try a new restaurant",
+    "Plan a trip",
+    "Cook something new",
+    "Find a podcast",
+    "Reach out to an old friend or relative",
+    "Rearrange furniture or redecorate",
+    "Go to a museum",
+    "Try learning a new language (or make your own!)",
+    "Make a home video",
+    "Have a photo shoot",
+    "Drive somewhere you've never been",
+    "Talk to someone",
+    "Exercise",
+    "Go for a walk",
+    "Organize something messy (desk, closet, pantry)",
+    "Update your resume",
+    "Clean something that hasn't been cleaned in a while",
+    "Meal prep for the week",
+    "Clean up your email inbox",
+    "Make a list of personal or professional goals",
+    "Start a DIY project",
+    "Go grocery shopping",
+    "Run an errand you need to do",
+    "Read up on current events",
+    "Clean your phone or phone case",
+    "Watch a how-to video",
+    "Clear out old clothes and donate them",
+    "Get ahead of any work you have",
+    "Do laundry",
+    "Make a list of something",
+    "Window shop online",
+    "Find your dream home in rent listings",
+    "Play a video game",
+    "Play a game with friends",
+    "Watch a documentary",
+    "Go to the movies",
+    "Write a love note",
+    "Watch YouTube videos",
+    "Make a bucket list (or add to a current one)",
+    "Plan a dream wedding (doesn't have to be yours)",
+    "Read a magazine",
+    "Go to a play or musical",
+    "Sing",
+    "Dance",
+    "Learn a new song",
+    "Go to a nearby event (Google one!)",
+    "Learn to code (or a new language if you already can)",
+    "Learn or study Morse Code",
+    "Research a random topic you're interested in",
+    "Work on a skill you have",
+    "Update your social profile",
+    "Build something (even just a couch fort!)",
+    "Scroll through a social site you like (Reddit, Instagram, Pinterest, ...)",
+    "Learn a new skill",
+    "Try writing or drawing with your non-dominant hand",
+    "Go somewhere to people watch",
+    "Practice or learn an instrument",
+    "Look for a new job (or fantasize about the perfect one)",
+    "Make a to do list",
+    "Research something that will help you at work",
+    "Wash your car (or whatever you use most to get around town)",
+    "Invent a game with a friend and play it"
+]
+var social = [
+    "Cuddle with someone",
+    "Go on a walk/hike with someone",
+    "Go out to a new restaurant with someone",
+    "Reach out to an old friend or relative",
+    "Go to a museum with someone",
+    "Make a home video",
+    "Drive somewhere you've never been",
+    "Talk to someone",
+    "Go grocery shopping",
+    "Run an errand any of you need to do",
+    "Play a game with friends (online or in person)",
+    "Go to the movies",
+    "Go to a play or musical",
+    "Sing",
+    "Dance",
+    "Go to a nearby event (Google one!)",
+    "Go somewhere to people watch",
+    "Invent a game together and play it",
+    "Play hide and seek in a big store or area"
+]
+var yourself = [
+    "Give yourself a manicure/pedicure",
+    "Experiment with your hair", 
+    "Draw", 
+    "Do yoga", 
+    "Write a poem, story, or journal entry", 
+    "Take a nap", 
+    "Read", 
+    "Listen to music", 
+    "Find a new band or artist", 
+    "Stretch", 
+    "Watch TV/Streaming service", 
+    "Look at old photos/videos", 
+    "Bake", 
+    "Meditate", 
+    "Have a snack", 
+    "Sit outside (if weather permits)",
+    "Write a letter to your future self", 
+    "Cuddle with something",
+    "Take yourself on a hike/walk",
+    "Take yourself to a restaurant you've been wanting to try",
+    "Plan a trip",
+    "Cook something new",
+    "Find a podcast",
+    "Rearrange furniture or redecorate",
+    "Take yourself to a museum",
+    "Try learning a new language (or make your own!)",
+    "Make a home video",
+    "Have a photo shoot",
+    "Drive somewhere you've never been",
+    "Exercise",
+    "Organize something messy (desk, closet, pantry)",
+    "Update your resume",
+    "Clean something that hasn't been cleaned in a while",
+    "Meal prep for the week",
+    "Clean up your email inbox",
+    "Make a list of personal or professional goals",
+    "Start a DIY project",
+    "Go grocery shopping",
+    "Run an errand you need to do",
+    "Read up on current events",
+    "Clean your phone or phone case",
+    "Watch a how-to video",
+    "Clear out old clothes and donate them",
+    "Get ahead of any work you have",
+    "Do laundry",
+    "Make a list of something",
+    "Window shop online",
+    "Find your dream home in rent listings",
+    "Play a video game",
+    "Watch a documentary",
+    "Take yourself to the movies",
+    "Write a love note (to yourself or someone you love)",
+    "Watch YouTube videos",
+    "Make a bucket list (or add to a current one)",
+    "Plan a dream wedding (doesn't have to be yours)",
+    "Read a magazine",
+    "Go to a play or musical",
+    "Sing",
+    "Dance",
+    "Learn a new song",
+    "Go to a nearby event (Google one!)",
+    "Learn to code (or a new language if you already can)",
+    "Learn or study Morse Code",
+    "Research a random topic you're interested in",
+    "Work on a skill you have",
+    "Update your social profile",
+    "Build something (even just a couch fort!)",
+    "Scroll through a social site you like (Reddit, Instagram, Pinterest, ...)",
+    "Learn a new skill",
+    "Try writing or drawing with your non-dominant hand",
+    "Go somewhere to people watch",
+    "Practice or learn an instrument",
+    "Look for a new job (or fantasize about the perfect one)",
+    "Make a to do list",
+    "Research something that will help you at work"
+]
+var inside = [
+    "Give yourself a manicure/pedicure",
+    "Experiment with your hair", 
+    "Draw", 
+    "Do yoga", 
+    "Write a poem, story, or journal entry", 
+    "Take a nap", 
+    "Read", 
+    "Listen to music", 
+    "Find a new band or artist", 
+    "Stretch", 
+    "Watch TV/Streaming service", 
+    "Look at old photos/videos", 
+    "Bake", 
+    "Meditate", 
+    "Have a snack", 
+    "Sit outside (if weather permits)",
+    "Write a letter to your future self",
+    "Cuddle with someone or something", 
+    "Plan a trip",
+    "Cook something new",
+    "Find a podcast",
+    "Reach out to an old friend or relative",
+    "Rearrange furniture or redecorate",
+    "Try learning a new language (or make your own!)",
+    "Make a home video",
+    "Have a photo shoot",
+    "Exercise",
+    "Organize something messy (desk, closet, pantry)",
+    "Update your resume",
+    "Clean something that hasn't been cleaned in a while",
+    "Meal prep for the week",
+    "Clean up your email inbox",
+    "Make a list of personal or professional goals",
+    "Start a DIY project",
+    "Read up on current events",
+    "Clean your phone or phone case",
+    "Watch a how-to video",
+    "Clear out old clothes and donate them",
+    "Get ahead of any work you have",
+    "Do laundry",
+    "Make a list of something",
+    "Window shop online",
+    "Find your dream home in rent listings",
+    "Play a video game",
+    "Play a game with friends",
+    "Watch a documentary",
+    "Watch a movie",
+    "Write a love note (to yourself or someone you love)",
+    "Watch YouTube videos",
+    "Make a bucket list (or add to a current one)",
+    "Plan a dream wedding (doesn't have to be yours)",
+    "Read a magazine",
+    "Sing",
+    "Dance",
+    "Learn a new song",
+    "Learn to code (or a new language if you already can)",
+    "Learn or study Morse Code",
+    "Research a random topic you're interested in",
+    "Work on a skill you have",
+    "Update your social profile",
+    "Build something (even just a couch fort!)",
+    "Scroll through a social site you like (Reddit, Instagram, Pinterest, ...)",
+    "Learn a new skill",
+    "Try writing or drawing with your non-dominant hand",
+    "Practice or learn an instrument",
+    "Look for a new job (or fantasize about the perfect one)",
+    "Make a to do list",
+    "Research something that will help you at work"
+]
+var outside = [
+    "Go for a hike",
+    "Try a new restaurant",
+    "Go to a museum",
+    "Drive somewhere you've never been",
+    "Talk to someone new",
+    "Exercise outside",
+    "Go grocery shopping",
+    "Run an errand you need to do",
+    "View houses/apartments for rent (for fun or for real)",
+    "Play a game outside with friends",
+    "Go to the movies",
+    "Go to a play or musical",
+    "Go to a nearby event (Google one!)",
+    "Go somewhere to people watch",
+    "Take a walk around a park or somewhere scenic",
+    "Wash your car (or whatever you use most to get around town)",
+    "Bird watch (or watch any animals nearby)"
+]
+
+var newArr = [];
+var Rtitle = "";
+
+function allIdeas()
+{
+    var rand = Math.floor(Math.random()*baseList.length);
+    document.getElementById("opt").innerHTML = baseList[rand];
+}
+
+function singleIdeas()
+{
+    var rand = Math.floor(Math.random()*yourself.length);
+    document.getElementById("opt").innerHTML = yourself[rand];
+}
+
+function socialIdeas()
+{
+    var rand = Math.floor(Math.random()*social.length);
+    document.getElementById("opt").innerHTML = social[rand];
+}
+
+function insideIdeas()
+{
+    var rand = Math.floor(Math.random()*inside.length);
+    document.getElementById("opt").innerHTML = inside[rand];
+}
+
+function outsideIdeas()
+{
+    var rand = Math.floor(Math.random()*outside.length);
+    document.getElementById("opt").innerHTML = outside[rand];
+}
+
+function addRoulette()
+{
+    Rtitle = document.getElementById("title").value;
+    var ideas = document.getElementById("ideas").value;
+    start = 0;
+    end = 0;
+    for(var i = 0; i < ideas.length; i++)
+    {
+        var idea = "";
+        if(ideas[i] == ";")
+        {
+            end = i;
+            for(var x = start; x < end; x++)
+            {
+                idea+= ideas[x];
+            }
+            newArr.push(idea);
+            idea = "";
+            start = end + 1;
+            if(ideas[start] == " ")
+            {
+                start++;
+            }
+        }
+        if(i == ideas.length-1 && ideas[i] !== ";")
+        {
+            end = ideas.length;
+            for(var x = start; x < end; x++)
+            {
+                idea+= ideas[x];
+            }
+            newArr.push(idea)
+        }
+    }
+    location.href = "myRoulettes.html";
+}
